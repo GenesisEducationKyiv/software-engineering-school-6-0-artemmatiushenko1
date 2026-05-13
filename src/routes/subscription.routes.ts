1a4f4f7fab9568e9c62fb55efb0ffee758abb7fb
artemmatiushenko1 <artom.matyushenko@gmail.com>
2026-05-13 22:14:31 +0200

    remove fastify request validation

--- src/routes/subscription.routes.ts
+++ src/routes/subscription.routes.ts
@@ -13,18 +13,6 @@ export const subscriptionRoutes: FastifyPluginAsync<
 
   fastify.post<{ Body: { email: string; repo: string } }>(
     '/subscribe',
-    {
-      schema: {
-        body: {
-          type: 'object',
-          required: ['email', 'repo'],
-          properties: {
-            email: { type: 'string' },
-            repo: { type: 'string' },
-          },
-        },
-      },
-    },
     async (request, reply) => {
       const { email, repo } = request.body;
       await subscriptionService.subscribe(email, repo);
@@ -36,17 +24,6 @@ export const subscriptionRoutes: FastifyPluginAsync<
 
   fastify.get<{ Querystring: { email: string } }>(
     '/subscriptions',
-    {
-      schema: {
-        querystring: {
-          type: 'object',
-          required: ['email'],
-          properties: {
-            email: { type: 'string' },
-          },
-        },
-      },
-    },
     async (request, reply) => {
       const { email } = request.query;
       const subscriptions =
@@ -59,17 +36,6 @@ export const subscriptionRoutes: FastifyPluginAsync<
 
   fastify.get<{ Params: { token: string } }>(
     '/confirm/:token',
-    {
-      schema: {
-        params: {
-          type: 'object',
-          required: ['token'],
-          properties: {
-            token: { type: 'string' },
-          },
-        },
-      },
-    },
     async (request, reply) => {
       const { token } = request.params;
       await subscriptionService.confirmSubscription(token);
@@ -81,17 +47,6 @@ export const subscriptionRoutes: FastifyPluginAsync<
 
   fastify.get<{ Params: { token: string } }>(
     '/unsubscribe/:token',
-    {
-      schema: {
-        params: {
-          type: 'object',
-          required: ['token'],
-          properties: {
-            token: { type: 'string' },
-          },
-        },
-      },
-    },
     async (request, reply) => {
       const { token } = request.params;
       await subscriptionService.unsubscribe(token);
