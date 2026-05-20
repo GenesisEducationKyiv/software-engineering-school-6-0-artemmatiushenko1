0f426b1fbc3ee345d731f20bb2d5b0acbbf1ba70
artemmatiushenko1 <artom.matyushenko@gmail.com>
2026-05-20 23:05:37 +0200

    exclude e2e in vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,6 @@ import { defineConfig, configDefaults } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    exclude: [...configDefaults.exclude, 'src/e2e/**'],
+    exclude: [...configDefaults.exclude, 'tests/e2e/**'],
   },
 });
